5f72bbc2e128f856b0537a0c09e9ff8ee79e91a4
Jayakrishnan B <Jayakrishnan.B@gds.ey.com>
2024-04-29 21:47:48 +0530

    commited changes

---
+++ src/main/java/listeners/Listeners.java
@@ -0,0 +1,14 @@
+package listeners;
+
+
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener, ISuiteListener {
+
+  private static final String MESSAGE = "Test - <b>";
+
+
+}
