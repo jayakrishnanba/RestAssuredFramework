92fd54c3fd01ac41df19a7887a6e6f40f241cb98
Jayakrishnan B <Jayakrishnan.B@gds.ey.com>
2024-04-29 21:52:20 +0530

    commited changes

--- src/main/java/listeners/Listeners.java
+++ src/main/java/listeners/Listeners.java
@@ -1,10 +1,10 @@
 package listeners;
 
 
-import org.testng.ISuite;
+
 import org.testng.ISuiteListener;
 import org.testng.ITestListener;
-import org.testng.ITestResult;
+
 
 public class Listeners implements ITestListener, ISuiteListener {
 
